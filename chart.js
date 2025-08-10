37b0cce305ea5df2c1174cb3595d9a75d80a4598
tomaszpio <tomaszpio@gmail.com>
2025-08-10 23:31:39 +0200

    Show only season labels in sunburst

--- chart.js
+++ chart.js
@@ -121,7 +121,7 @@ Promise.all([
       .attr('text-anchor', 'middle')
       .style('user-select', 'none')
       .selectAll('text')
-      .data(root.descendants().slice(1))
+      .data(root.descendants().slice(1).filter(d => d.depth === 1))
       .join('text')
         .attr('dy', '0.35em');
 
